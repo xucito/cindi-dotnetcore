1bc6c90f7b1a3ffc370e6647e8ccc66521470747
Tony Nguyen <tnguyen@etisalat.ae>
2018-10-20 15:09:00 +0400

    Add Clients

---
+++ Cindi.DotNetCore/Cindi.DotNetCore/Client/CindiClient.cs
@@ -0,0 +1,38 @@
+﻿using Cindi.DotNetCore.BotExtensions.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cindi.DotNetCore.BotExtensions.Client
+{
+    public class CindiClient
+    {
+        private string _url;
+
+        public CindiClient(string url)
+        {
+            _url = url;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input">Returns task Id</param>
+        /// <returns></returns>
+        public async Task<int> PostNewSequence(SequenceInput input)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+
+                var result = await client.PostAsync("/sequences", new StringContent(JsonConvert.SerializeObject(input), Encoding.UTF8, "application/json"));
+
+                var test = result.Content;
+
+                return 1;
+            }
+        }
+    }
+}
---
+++ Cindi.DotNetCore/Cindi.DotNetCore/Models/SequenceInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cindi.DotNetCore.BotExtensions.Models
+{
+    public class SequenceInput
+    {
+        public string Name { get; set; }
+        public TemplateReference SequenceTemplateReference { get; set; }
+        public List<CommonData> InputData { get; set; }
+    }
+}
